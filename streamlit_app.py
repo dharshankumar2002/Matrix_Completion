1a6348408bde03c5a4291eeb59c17c5d62dcfbdb
Dharshan Kumar <52622845+dharshankumar2002@users.noreply.github.com>
2021-12-21 00:50:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,10 @@ st.write("This is a simple matrix completion calculator")
 
 # first argument shows the title of the text input box
 # second argument displays a default text inside the text input area
-name = st.text_input("Enter Your Matrix", "Type Here ...")
+name = st.text_input("Enter Your Matrix", "")
  
 # display the name when the submit button is clicked
 # .title() is used to get the input text string
 if(st.button('Submit')):
-    result = name.title()
+    result = "Wait a minute..."
     st.success(result)
