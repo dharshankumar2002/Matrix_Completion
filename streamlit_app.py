01db823305efce5792577da64c3a578be70f3129
Dharshan Kumar <52622845+dharshankumar2002@users.noreply.github.com>
2021-12-21 01:10:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,18 @@ name = st.text_input("Enter Your Matrix", "")
  
 status = st.radio("Select Algotihm: ", ('Singular Value Thresholding', 'Gradient Descent', 'Augmented Lagrange Multipliers'))
          
+seconds = 0
+diff = 0
 # display the name when the submit button is clicked
 # .title() is used to get the input text string
 if(st.button('Submit')):
-    result = "Wait a minute..."
-    st.success(result)
-         
+    while(diff<4):
+             result = "Wait a minute..."
+             st.success(result)
+             seconds2 = time.time()
+             diff = seconds - seconds2
+             seconds = seconds2
+
     mat = [[1,2,3],[4,5,6],[7,8,9]]
+    mat = name.title()
     st.write(str(mat))
