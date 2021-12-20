88c2553ff4c88f7fb628089e9898f1fdb5473b65
Dharshan Kumar <52622845+dharshankumar2002@users.noreply.github.com>
2021-12-21 00:58:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,5 @@ if(st.button('Submit')):
     result = "Wait a minute..."
     st.success(result)
          
-    mat = [1 2 3; 4 5 6; 7 8 9]
+    mat = [[1,2,3],[4,5,6],[7,8,9]]
     st.write(mat)
